bbc45b1cc39ffc6cc4b871759b6816b39f664bbc
lijinbao3 <lijinbao8@jd.com>
2015-09-23 16:35:28 +0800

    二次修改提交

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,4 +1,13 @@
 
 public class TestGit {
+	private String testProperty;
 
+	public String getTestProperty() {
+		return testProperty;
+	}
+
+	public void setTestProperty(String testProperty) {
+		this.testProperty = testProperty;
+	}
+	
 }
