054a73105e534b393ca3c3006d3e38df948fae72
lijinbao3 <lijinbao8@jd.com>
2015-09-25 11:09:16 +0800

    fen zhi dao zhugan

--- src/TestGit.java
+++ src/TestGit.java
@@ -11,7 +11,7 @@ public class TestGit {
 	}
 	private void sys() {
 		// TODO Auto-generated method stub
-		System.out.println("第一。。。。");
+		System.out.println("第一。。。。wo ziji 44344444de fenzhi");
 	}
 	
 }
