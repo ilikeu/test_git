f6aa79360f7268d583f302447adb25614c827f10
lijinbao3 <lijinbao8@jd.com>
2015-09-29 16:52:59 +0800

    test commit

--- src/TestGit.java
+++ src/TestGit.java
@@ -11,7 +11,7 @@ public class TestGit {
 	}
 	private void sys() {
 		// TODO Auto-generated method stub
-		System.out.println("第一。。。。wo ziji 44344444de fenzhi");
+		System.out.println("第一客户端");
 	}
 	
 }
